7d09a175c1c9e3e40f2c0f6cb9c88bcea0d5faa6
ashaw0724 <shawa8@rpi.edu>
2024-03-04 12:31:31 -0500

    Sleep Optimization and Health Education
    Completion of sleep optimization and health education pages. Connection of profile pages and health log to main infrastructure. Sleep optimization time calculations work. Read buttons currently do nothing on health education page, may include modal boxes later for pop up articles.

---
+++ pages/javascript/sleep.js
@@ -0,0 +1,31 @@
+function link(dest) {
+  window.location.href = dest + ".html";
+}
+
+function getTime() {
+  let days = ["Su", "M", "Tu", "W","Th", "F","Sa"];
+  let date = new Date();
+  var dow = date.getDay();
+  let time = document.getElementById("bedTime").value;
+  let [hours, mins] = time.split(":");
+  if (hours > 12) {
+    document.getElementById(days[dow] + "BedTime").innerHTML = hours%12 + ":" + mins + " PM";
+  }
+  else {
+      document.getElementById(days[dow] + "BedTime").innerHTML = hours + ":" + mins + " PM";
+  }
+
+  let Wtime = document.getElementById("wakeTime").value;
+  let [Whours, Wmins] = Wtime.split(":");
+  if (Whours > 12) {
+      document.getElementById(days[dow] + "WakeTime").innerHTML = Whours%12 + ":" + Whoursmins + " PM";
+    
+  }
+  else {
+    document.getElementById(days[dow] + "WakeTime").innerHTML = Whours + ":" + Wmins + " AM";
+  }
+
+  let calculate = (hours%12 - Whours) / 36e5;
+  let sleeping = String(calculate);
+  document.getElementById(days[dow] + "CalculateTime").innerHTML = "<strong>" + sleeping.split(".")[0] + " hours </strong>";
+}
