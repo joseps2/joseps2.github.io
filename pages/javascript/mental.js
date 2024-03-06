3610b0ac730b0f8a9e27af873874917f802a8e5c
ashaw0724 <shawa8@rpi.edu>
2024-03-06 15:45:14 -0500

    Nutrition Tracker Filters
    Modal filters for nutrition trackers, next step is creating a JSON and pulling recipe info dynamically.

---
+++ pages/javascript/mental.js
@@ -0,0 +1,36 @@
+function link(dest) {
+    window.location.href = dest + ".html";
+}
+
+
+function startMeditation(meditationType) {
+    console.log(`Starting ${meditationType} meditation`);
+}
+
+function loadMindfulnessLibrary() {
+    const library = [
+        { name: 'Bedtime Meditation', duration: '10 minutes' },
+        { name: 'Deep Breathing Exercises', duration: '15 minutes' },
+        { name: 'Quick Meditation Session', duration: '5 minutes' }
+    ];
+
+    const libraryElement = document.querySelector('.mindfulnessLibrary ul');
+    libraryElement.innerHTML = '';
+
+    library.forEach(item => {
+        const listItem = document.createElement('li');
+        listItem.textContent = `${item.name} - Time: ${item.duration}`;
+        listItem.onclick = () => startMeditation(item.name);
+        libraryElement.appendChild(listItem);
+    });
+}
+
+
+document.addEventListener('DOMContentLoaded', () => {
+    loadMindfulnessLibrary(); 
+
+    const dailyExerciseButton = document.querySelector('.dailyMindfulness');
+    if (dailyExerciseButton) {
+        dailyExerciseButton.addEventListener('click', () => startMeditation('Guided Meditation'));
+    }
+});
--- pages/javascript/nutrition.js
+++ pages/javascript/nutrition.js
@@ -1,3 +1,29 @@
 function link(dest) {
   window.location.href = dest + ".html";
 }
+
+// Get the modal
+var modal = document.getElementById("filterModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("filterBtn");
+
+// Get the <span> element that closes the modal
+var closeBtn = document.getElementById("cancel");
+
+// When the user clicks on the button, open the modal
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+closeBtn.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
