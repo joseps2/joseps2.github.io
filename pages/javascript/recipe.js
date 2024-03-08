b28fd1c3a4d6fbcf70444e79a08c6fea77be0c14
ashaw0724 <shawa8@rpi.edu>
2024-03-08 10:20:23 -0500

    Nutrition Tracker Recipes
    Nutrition tracker page complete. Clicking view recipe will show corresponding recipe information loaded dynamically from a JSON file.

---
+++ pages/javascript/recipe.js
@@ -0,0 +1,40 @@
+function link(dest) {
+  window.location.href = dest + ".html";
+}
+
+/*Enact API calls based on the user's position */
+function funcCall() {
+  var index = localStorage.getItem("arrIndex");
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      var obj = JSON.parse(this.responseText);
+        document.getElementById("colOne").innerHTML += "<img src='" + obj.recipes[index].Image + "' alt='" + obj.recipes[index].Image + "'class='recipeImg'>";
+        document.getElementById("colOne").innerHTML += "<h3>" + obj.recipes[index].title + "</h3>";
+        document.getElementById("colOne").innerHTML += "<p class='pRecipe'><strong>Cook Time: </strong>" + obj.recipes[index].cookTime + " Minutes</p>";
+        document.getElementById("colOne").innerHTML += "<p class='pRecipe'><strong>Allergens: </strong>" + obj.recipes[index].Allergens + "</p>";
+        document.getElementById("colOne").innerHTML += "<p class='pRecipe'><strong>Allergens: </strong>" + obj.recipes[index].Servings + "</p>";
+        document.getElementById("colOne").innerHTML += "<i class='fa-solid fa-share-nodes'></i>";
+        var ingre = obj.recipes[index].Ingredients.split(',');
+        document.getElementById("colTwo").innerHTML += "<ul>";
+        for (let i = 0; i < ingre.length; i++) {
+          document.getElementById("colTwo").innerHTML += "<li>" + ingre[i] + "</li>";
+          if (i+1 == ingre.length) {
+            document.getElementById("colTwo").innerHTML += "</ul>";
+          }
+        }
+        var recp = obj.recipes[index].Recipe.split('.');
+        document.getElementById("colThree").innerHTML += "<ol type='1'>";
+        for (let i = 0; i < recp.length; i++) {
+          document.getElementById("colThree").innerHTML += "<li>" + recp[i] + "</li>";
+          if (i+1 == recp.length) {
+            document.getElementById("colThree").innerHTML += "</ol>";
+          }
+        }
+        
+    }
+  };
+  var link = "../pages/recipes.json";
+  xhttp.open("GET", link);
+  xhttp.send();
+}
